ab3c9b091a156886e8e1d4bf2daa294fad5ef638
Jakub Tancula <kuba.tancula@gmail.com>
2017-05-28 15:23:18 +0200

    branch test

---
+++ batch_file.bat
@@ -0,0 +1,11 @@
+@echo off
+SET mypath=%~dp0
+
+set PATH=C:\Windows\system32
+set PATH=%PATH%;D:\Program Files\Python27_64
+set PATH=%PATH%;D:\Program Files\Python27_64\Scripts
+set PATH=%PATH%;D:\MinGW\bin
+set PATH=%PATH%;D:\MinGW\libexec\gcc\mingw32\4.9.3
+set PATH=%PATH%;D:\Program Files\Git\bin
+cd %mypath
+cmd
